1ff683284a2ebb1dc993c3af0262218bf6f6ebcc
federico alegre <fedealegre@gmail.com>
2022-05-09 17:15:28 -0300

    Agregar archivos de proyecto.

---
+++ Bootcamp_6/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime;
+
+namespace Bootcamp_6
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public DateTime birthDate { get; set; }
+
+        public Persona(string nombre, int year, int month, int day)
+        {
+            this.birthDate = new DateTime(year, month, day);
+            this.nombre = nombre;
+        }
+    }
+
+    public static class Extensiones
+    {
+        public static void esMayor(this Persona p)
+        {
+            DateTime hoy = new DateTime(2022, 05, 09);
+            int edad = hoy.Year - p.birthDate.Year;
+            if ( edad < 18)
+            {
+                Console.WriteLine($"{p.nombre} es menor de edad");
+            }
+            else Console.WriteLine($"{p.nombre} es mayor de edad");
+        }
+
+        public static void centavos(this double d)
+        {
+            string s = d.ToString();
+            string[] subs = s.Split(',');
+            Console.WriteLine($"Centavos: {subs[1]}");
+        }
+        public static void parteEntera(this double d)
+        {
+            string s = d.ToString();
+            string[] subs = s.Split(',');
+            Console.WriteLine($"Parte entera: {subs[0]}");
+        }
+    }
+}
---
+++ Bootcamp_6/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Threading.Tasks;
+//using System.Collections;
+
+namespace Bootcamp_6
+{
+    class Program
+    {
+        /*public static async Task proces()
+        {
+
+        }*/
+
+        static void Ejercicio9()
+        {
+            Persona p = new Persona("fede", 2002, 09, 11);
+            p.esMayor();
+            
+        }
+        static void Ejercicio12()
+        {
+            double d = 12.25;
+            d.parteEntera();
+            d.centavos();
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("·Bootcamp 6·");
+
+            Ejercicio12();
+
+            Ejercicio9();
+
+            Rectangulo r = new Rectangulo(3, 2);
+
+            Console.WriteLine($"Perimetro: {r.getPerimetro()} cm\nArea: {r.getArea()} cm cuadrados");
+
+            Stack<Rectangulo> rec = new Stack<Rectangulo>(3);
+            rec.push(r);
+            rec.push(r);
+            Console.WriteLine(rec.pop().ToString());
+
+            Console.WriteLine(rec.pop().ToString());
+            
+            Stack<string> s = new Stack<string>(2);
+            s.push("fede");
+            s.push("soy");
+            Console.WriteLine(s.pop());
+            
+            Console.WriteLine(s.pop());       
+        }
+    }
+
+    public partial class Rectangulo
+    {
+        private int lado1;
+        private int lado2;
+
+        public int Lado1
+        {
+            get
+            {
+                return lado1;
+            }
+            set
+            {
+                lado1 = value;
+            }
+        }
+        public int Lado2
+        {
+            get
+            {
+                return lado2;
+            }
+            set
+            {
+                lado2 = value;
+            }
+        }
+
+        public Rectangulo(int lado1, int lado2 )
+        {
+            this.lado1 = lado1;
+            this.lado2 = lado2;
+        }
+
+    }
+
+    public partial class Rectangulo
+    {
+        public int getPerimetro()
+        {
+            return Lado1 * 2 + Lado2 * 2;
+        }
+
+        public int getArea()
+        {
+            return Lado1 * lado2;
+        }
+
+        public override string ToString()
+        {
+            return $"Lado 1: {Lado1}\nLado 2: {Lado2}\nArea: {getArea()}\nPerimetro: {getPerimetro()}";
+        }
+    }
+
+    public class Stack<T>
+    {
+        public T[] data { get; set; }
+        public int i = 0;
+
+        public Stack(int x)
+        {
+            data = new T[x];
+        }
+
+        public void push(T d)
+        {
+            data[i] = d;
+            i++;
+        }
+
+        public T pop()
+        {
+            i--;
+            return data[i];
+            
+        }
+    }
+}
